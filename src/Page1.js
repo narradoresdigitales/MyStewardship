3802f5f2b8d1c27049eff985c0ca4f6df5e084f9
sil <narradoresdigitales@gmail.com>
2025-02-11 06:09:13 -0600

    updated page1.js to add LearningHub

--- src/Page1.js
+++ src/Page1.js
@@ -13,7 +13,7 @@ function Page1() {
     const mindfulnessDiv = createElement('div', {
       className: 'section mindfulness' }, [
 
-      createElement('h3', {textContent: 'About Mindfulness'}),
+      createElement('h3', {textContent: 'About RecycleGeo'}),
       createElement('p', {textContent: 'Here I talk about mindfulness practices in stewardship'}),
       createElement('a', {
         href: '/#/page2',
@@ -36,9 +36,21 @@ function Page1() {
     ]
   );
 
+// Eco-Friendly <div>
+
+const LearningHub = createElement('div', {
+  className: 'section eco-friendly'}, [
+    createElement('h3', {textContent: 'About LearningHUb practices'}),
+    createElement('p', {textContent: 'Here I talk about mindfulness practices in being eco-friendly'}),
+    createElement('a', {
+      href: '/#/page4',
+      textContent: 'Go to Page 4'
+    })
+  ]
+);
 
 
-  return createElement('div', {}, [title, mindfulnessDiv, ecofriendlyDiv]);
+  return createElement('div', {}, [title, mindfulnessDiv, ecofriendlyDiv, LearningHub]);
 
 
 }
