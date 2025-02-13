b88fcd06742a6fdc8c905ba6feb95b1a60400868
sil <narradoresdigitales@gmail.com>
2025-02-13 06:09:00 -0600

    added gamify feature and began to style it

--- src/Page5.js
+++ src/Page5.js
@@ -1,15 +1,23 @@
 import { createElement } from './utils';
+import { initEcoHero } from './ecoHero';
 
 function Page5() {
-  
-  
     const title = createElement('h2', { 
-    textContent: 'Becoming an Eco-Hero' });
+        textContent: 'Becoming an Eco-Hero' 
+    });
 
+    const appContainer = createElement('div', { id: 'app' });
 
-    return createElement('div', {}, [title]);
+    // Return the container and title elements
+    const pageElement = createElement('div', {}, [title, appContainer]);
 
+    // Initialize eco-hero functionality after the page is rendered
+    // Ensure the app container is part of the DOM before calling initEcoHero
+    window.onload = () => {
+        initEcoHero(); 
+    };
 
+    return pageElement;
 }
 
-export default Page5; 
+export default Page5;
